fcfb2543b7891d5b9c427d498145fae263e3deb9
Archiraph <raphael.mortiers@hotmail.com>
2023-12-22 16:40:14 +0100

    Redux_posts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles/index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// Redux
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers";
+import { getPosts } from "./actions/post.action";
+import { getUser } from "./actions/user.action";
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
+store.dispatch(getPosts());
+store.dispatch(getUser());
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
